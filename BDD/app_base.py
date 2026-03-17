67f05ab8a4027820ddb57a36c2af1ae7bfee570d
Vasily Nemkov <V.Nemkov@gmail.com>
2026-03-17 23:37:14 +0100

    minor: comment

--- BDD/app_base.py
+++ BDD/app_base.py
@@ -166,7 +166,7 @@ class GStreamerApp:
         self.loop = None
         self.threads = []
         self.error_occurred = False
-        self.pipeline_latency = 0  # milliseconds; 0 = use each element's natural minimum latency
+        self.pipeline_latency = 0  # milliseconds; 0 = use each element's natural minimum latency; if frames are dropped or Gstreamer is stalled, then set to 50
 
         # Set Hailo parameters; these parameters should be set based on the model used
         self.batch_size = 1
