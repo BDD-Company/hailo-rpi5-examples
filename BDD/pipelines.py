184a0b0788fb2488ec5c8d3db8e9472b85f5f494
Alexei Yanochkin <alexei.yanochkin@seranking.com>
2026-03-27 16:50:55 +0100

    TMP Fix for test

--- BDD/pipelines.py
+++ BDD/pipelines.py
@@ -113,7 +113,6 @@ def SOURCE_PIPELINE(video_source, video_width=640, video_height=640,
     else:
         source_element = (
             f'filesrc location="{video_source}" name={name} ! '
-            f'{QUEUE(name=f"{name}_queue_decode")} ! '
             f'decodebin name={name}_decodebin ! '
         )
 
