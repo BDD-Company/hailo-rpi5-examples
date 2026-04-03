4ab0029b7ad6c249b3a182502652f2e739342756
Vasily Nemkov <V.Nemkov@gmail.com>
2026-04-04 00:07:08 +0200

    Better environment setup scripts

--- scripts/bdd.sh
+++ scripts/bdd.sh
@@ -1,12 +1,32 @@
 #! /usr/bin/env bash
 
 cd ./hailo-rpi5-examples/
-. ./setup_env.sh 
+. ./setup_env.sh
 
-git -P status ||:
-git -P diff ||:
-
+readonly start_time=$(date +%Y%m%d-%H%M%S)
 set -ex
 
-DISPLAY=:0 python ./BDD/app.py --hef-path /home/bdd/models/2026-02-18_n640-last.hef -i rpi -f -r 60 "$@" 
+(
+    if [[ -d .git ]];
+    then
+        git -P log -n1 --oneline
+        git -P describe --long --always --dirty ||:
+        git -P diff ||:
+    fi
 
+#    export G_MESSAGES_DEBUG=all
+#    export GST_TRACERS="latency;stats"
+#    export GST_DEBUG="7"
+#    export GST_DEBUG_FILE=_DEBUG/gstreamer_${start_time}.log
+#    export GST_DEBUG_NO_COLOR=1
+    export DISPLAY=:0
+#    export HAILO_MODEL="/home/bdd/models/2026-03-13_shahed_v7_1280.hef"
+    export HAILO_MODEL="/home/bdd/models/2026-03-13_shahed_v7_1280.hef"
+
+    python \
+        ./BDD/app.py \
+        -i rpi \
+        --hef-path="${HAILO_MODEL}" \
+        -r 30 \
+        "$@"
+) 2>&1 | tee "./_DEBUG/BDD_${start_time}.log"
--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env bash
+# supposed to be executed at project root
+set -ex
 
-source ../setup_venv.sh
+
+if [[ ! -d ./venv_hailo_rpi_examples ]]; then
+    ./install.sh
+fi
+
+. ./setup_env.sh
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-requirements_file="$script_dir/requirements.txt"
+requirements_file="./requirements.txt"
 installed_requirements_file="${requirements_file}_INSTALLED"
 
 if [[ ! -f "$installed_requirements_file" ]] || ! cmp -s "$requirements_file" "$installed_requirements_file";
