1d192c5553ae20253462aad501fd6345aaca766a
Vasily Nemkov <V.Nemkov@gmail.com>
2026-04-24 18:03:38 +0200

    Updated script to for case of symbolic link and tmpfs

--- scripts/bdd.sh
+++ scripts/bdd.sh
@@ -1,18 +1,21 @@
 #! /usr/bin/env bash
 
+set -ex
+echo `pwd`
+
 cd ./hailo-rpi5-examples/
-. ./setup_env.sh
 
 readonly start_time=$(date +%Y%m%d-%H%M%S)
 # make sure that log dir exists
 mkdir -p ./_DEBUG/ ||:
 
-set -ex
 
 (
-    if [[ -d .git ]];
-    then
-        git -P log -n1 --oneline
+    #. ./setup_env.sh
+    . ./venv_hailo_rpi_examples/bin/activate
+
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+        git -P log -n1 --oneline ||:
         git -P describe --long --always --dirty ||:
         git -P diff ||:
     fi
@@ -22,15 +25,15 @@ set -ex
 #    export GST_DEBUG="GST_TRACER:7"
     export DISPLAY=:0
     #export HAILO_MODEL="/home/bdd/models/2026-04-07_11n_ball_v2.hef"
-    export HAILO_MODEL="/home/bdd/models/ball_11n_640_v1.hef" # detects SUN
-    #export HAILO_MODEL="/home/bdd/models/2026-04-13_11n_sh_v3.hef" # detects SUN
+    # export HAILO_MODEL="/home/bdd/models/ball_11n_640_v1.hef" # detects SUN
+    export HAILO_MODEL="/home/bdd/models/2026-04-18_11n_sh_v4.hef" # excludes Agro drone
 
     [[ -e "${HAILO_MODEL}" ]] || { echo "HAILO_MODEL does not exist: ${HAILO_MODEL}" >&2; exit 1; }
 
     # bind to 3rd core, 2 is bound to px4, effective only IF `isolcpus=2,3` is set in cmdline.txt
-    sudo taskset -c 3 \
+    taskset -c 3 \
         python \
-            ./BDD/app.py \
+            BDD/app.py \
             --hef-path "${HAILO_MODEL}" \
             -i rpi \
             "$@"
