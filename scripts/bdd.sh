032082976c6dc1065f6b85d4e21dff1d1ba0b49d
Vasily Nemkov <V.Nemkov@gmail.com>
2026-04-25 11:19:24 +0200

    better handling of the logs, with hpe of not stalling the process

--- scripts/bdd.sh
+++ scripts/bdd.sh
@@ -10,19 +10,12 @@ mkdir -p ./_DEBUG/ ||:
 set -ex
 
 (
-    if [[ -d .git ]];
-    then
-        git -P log -n1 --oneline
-        git -P describe --long --always --dirty ||:
-        git -P diff ||:
-    fi
-
 #    export G_MESSAGES_DEBUG=all
 #    export GST_TRACERS="latency;stats"
 #    export GST_DEBUG="GST_TRACER:7"
     export DISPLAY=:0
-    #export HAILO_MODEL="/home/bdd/models/2026-04-07_11n_ball_v2.hef"
-    export HAILO_MODEL="/home/bdd/models/ball_11n_640_v1.hef" # detects SUN
+    export HAILO_MODEL="/home/bdd/models/2026-04-07_11n_ball_v2.hef"
+    #export HAILO_MODEL="/home/bdd/models/ball_11n_640_v1.hef" # detects SUN
     #export HAILO_MODEL="/home/bdd/models/2026-04-13_11n_sh_v3.hef" # detects SUN
 
 
@@ -32,4 +25,7 @@ set -ex
         -i rpi \
         "$@"
 
-) 2>&1 | tee "./_DEBUG/BDD_${start_time}.log"
+) &> "./_DEBUG/BDD_${start_time}.log" &
+
+tail -f "./_DEBUG/BDD_${start_time}.log"
+
